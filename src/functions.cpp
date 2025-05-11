b72b10cd3bb985b61a3a93bbead5739cd676f02f
GabrielVioli <gabrielgvo16@gmail.com>
2025-05-11 16:29:14 -0300

    adicionado funcao para mostrar livro especifico

--- src/functions.cpp
+++ src/functions.cpp
@@ -19,6 +19,13 @@ string type_it;
 string opt, opt_t;
 int opt_3;
 
+struct livros {
+    string titulo;
+    string autor;
+    string genero;
+    int ano_pub;
+};
+
 void WriteFile(string x) {
     fstream WriteFil("data/library_data.txt", std::ios::app);
     if (WriteFil) {
@@ -44,30 +51,86 @@ string ListText() {
     ifstream inputFile(filename);
     string line;
     if(!inputFile) {
-        cerr<<"Erro ao abrir o arquivo";
-        return "erro ao abrir o arquivo";
+        cerr<<"Erro ao abrir o arquivo\n";
     }
 
     while (getline(inputFile, line)) {
-        cout<<line <<"\n";
+        cout<<line<<"\n";
     }
 
     inputFile.close();
-    
 }
 
+void Showbook() {
+    string filename = "data/library_data.txt"; //caminho do arquivo
+    ifstream file(filename); //tranforma o arquivo pra leitura
+    vector<string> lines; //cria um vetor onde será armazenado todas as linhas
+    string line, bookremover; //uma variavel onde será armazenada cada linha do arquivo
+    bool bookfound = false; //vericacao se o livro foi encontrado, inciando com false
+
+    if(!file) {
+        cout<<"erro ao abrir arquivo";
+    }
+
+    cout<<"digite o nome do livro que deseja ver: ";
+    getline(cin, bookremover);
+    
+    system("cls");
+    while(getline(file, line)) {
+        if(line == "titulo: " + bookremover) {
+            cout << line << "\n";
+            bookfound = true;
+
+            for(int i = 0; i<3; i++) {
+                getline(file, line);
+                cout << line << "\n";
+            }
+            cout<<"\n";
+            break;
+            
+        } 
+    }
+
+    file.close();
+
+    if (!bookfound) {
+        cout << "Livro não encontrado.\n";
+    }
+}
+
+
 void ShowMenu() {
-    cout<<"=======MENU PRINCIPAL=======\n[1] Adicionar\n[2] Remover\n[3] Listar\n[4] Sair\n";
-    cout<<"Opcao: "; cin>>opt_3;
+    cout << "======== GERENCIAMENTO DE LIVROS ========\n";
+    cout << "============= MENU PRINCIPAL ============\n";
+    cout << "[1] Adicionar livro\n";
+    cout << "[2] Remover livro\n";
+    cout << "[3] Listar todos os livros\n";
+    cout << "[4] Mostrar livro especifico\n";
+    cout << "[5] Sair\n";
+    cout << "Opcao: "; 
+    cin >> opt_3;
     cin.ignore();
 }
 
 void WriteMenu() {
     if(opt_3 == 1) {
         system("cls");
-        cout<<"titulo: ";
-        getline(cin, type_it);
-        WriteFile(type_it);
+        vector<livros> lista_livros;
+        livros livro;
+
+        cout<<"titulo: "; getline(cin, livro.titulo);
+        cout<<"genero: "; getline(cin, livro.genero);
+        cout<<"autor: "; getline(cin, livro.autor);
+        cout<<"ano de pubicação: "; cin>>livro.ano_pub; cin.ignore();
+        lista_livros.push_back(livro);
+
+        for(int i = 0; i < lista_livros.size(); i++) {
+            WriteFile("titulo: " + lista_livros[i].titulo);
+            WriteFile("autor: " + lista_livros[i].autor);
+            WriteFile("genero: " + lista_livros[i].genero);
+            WriteFile("Ano de publicacao: " + to_string(lista_livros[i].ano_pub));
+            WriteFile(" ");
+        }
     }
 }
 
@@ -87,3 +150,6 @@ void listShow() {
     }
 }
 
+void menushowbook() {
+    Showbook();
+}
